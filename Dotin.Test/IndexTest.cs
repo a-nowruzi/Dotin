e588d4c1595dbcbfedf867de7b07327561ee2597
a-nowruzi <09171067163n@gmail.com>
2024-01-14 05:22:05 +0330

    feat: add index test

---
+++ Dotin.Test/IndexTest.cs
@@ -0,0 +1,15 @@
+﻿using Bunit;
+
+namespace Dotin.Test
+{
+    public class IndexTest
+    {
+        [Fact]
+        public void RendersSuccessfully()
+        {
+            using var ctx = new TestContext();
+            var component = ctx.RenderComponent<Dotin.Client.Pages.Index>();
+            Assert.Equal("Add New User", component.Find($".btn").TextContent);
+        }
+    }
+}
